52f7abe0793d1910bb728322102495b816afc8c5
David <davidschlachter96@gmail.com>
2021-07-30 14:45:15 -0700

    initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "com.javatar.plugins"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
